458d92f693eaeb3cef22c93f8e8c5ee2c54faffb
shofiyul fuad <shofiyulfuad797@gmail.com>
2024-07-27 16:58:29 +0700

    commit pren

---
+++ components/MainHome.jsx
@@ -0,0 +1,182 @@
+import Link from "next/link";
+import Logo from "./Logo";
+import { useSelector } from "react-redux";
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+const MainHome = () => {
+  const { theme } = useSelector((state) => state.counterSlice);
+  return (
+    <motion.div
+      initial={{ y: 100, opacity: 0 }}
+      animate={{ y: [100, 0], opacity: [0, 1] }}
+      transition={{ duration: 0.7 }}
+      className="pb-12">
+      <div className="flex flex-col gap-5 pb-5 border-b border-b-slate-300">
+        <div className="flex flex-col gap-2">
+          <h1 className="flex text-2xl font-semibold 2md:text-3xl">
+            Hi, Iam Shofiyul Fuad
+            <span className="shake">👋</span>
+          </h1>
+          <div className="flex gap-6">
+            <li>front end developer</li>
+            <li>robotic</li>
+          </div>
+        </div>
+        <p className="text-justify">
+          I am a graduate of Electrical Engineering from Trunojoyo University. I
+          have a strong interest in web programming, and I am currently focusing
+          on improving my skills in front-end development.
+        </p>
+      </div>
+      <div className="flex flex-col gap-2 py-5">
+        <div className="flex items-center gap-2 mb-5 text-xl">
+          <Logo name="code" size={20} />
+          <h1 className="font-semibold">Skills</h1>
+        </div>
+        <div className="flex flex-col gap-3">
+          <div className="flex flex-col gap-1">
+            <h1 className="text-sm">Skills Web</h1>
+          </div>
+          <div>
+            <ul className="grid justify-between grid-cols-4 2lg:flex">
+              <li className="flex flex-col items-center gap-2 pb-2 group/html">
+                <Logo name="HTML5" size={40} />
+                <h1 className="text-xs transition-all duration-300 -translate-y-full opacity-0 group-hover/html:opacity-100 group-hover/html:translate-y-0">
+                  HTML5
+                </h1>
+              </li>
+              <li className="flex flex-col items-center gap-2 pb-2 group/css">
+                <Logo name="CSS" size={40} />
+                <h1 className="text-xs transition-all duration-300 -translate-y-full opacity-0 group-hover/css:opacity-100 group-hover/css:translate-y-0">
+                  CSS
+                </h1>
+              </li>
+              <li className="flex flex-col items-center gap-2 pb-2 group/js">
+                <Logo name="JavaScript" size={40} />
+                <h1 className="text-xs transition-all duration-300 -translate-y-full opacity-0 group-hover/js:opacity-100 group-hover/js:translate-y-0">
+                  Javascript
+                </h1>
+              </li>
+              <li className="flex flex-col items-center gap-2 pb-2 group/php">
+                <Logo name="PHP" size={40} />
+                <h1 className="text-xs transition-all duration-300 -translate-y-full opacity-0 group-hover/php:opacity-100 group-hover/php:translate-y-0">
+                  PHP
+                </h1>
+              </li>
+              <li className="flex flex-col items-center gap-2 pb-2 group/react">
+                <Logo name="React.js" size={35} />
+                <h1 className="text-xs transition-all duration-300 -translate-y-full opacity-0 group-hover/react:opacity-100 group-hover/react:translate-y-0">
+                  React.js
+                </h1>
+              </li>
+              <li className="flex flex-col items-center gap-2 pb-2 group/next">
+                <Logo name="Next.js" size={35} />
+                <h1 className="text-xs transition-all duration-300 -translate-y-full opacity-0 group-hover/next:opacity-100 group-hover/next:translate-y-0">
+                  Next.js
+                </h1>
+              </li>
+              <li className="flex flex-col items-center gap-2 pb-2 group/laravel">
+                <Logo name="Laravel" size={35} />
+                <h1 className="text-xs transition-all duration-300 -translate-y-full opacity-0 group-hover/laravel:opacity-100 group-hover/laravel:translate-y-0">
+                  Laravel
+                </h1>
+              </li>
+              <li className="flex flex-col items-center gap-2 pb-2 group/tail">
+                <Logo name="Tailwindcss" size={40} />
+                <h1 className="text-xs transition-all duration-300 -translate-y-full opacity-0 group-hover/tail:opacity-100 group-hover/tail:translate-y-0">
+                  Tailwind
+                </h1>
+              </li>
+              {/* <li className="flex flex-col items-center gap-2 pb-2 group/mongo">
+                <Logo name="Mongodb" size={40} />
+                <h1 className="text-xs transition-all duration-300 -translate-y-full opacity-0 group-hover/mongo:opacity-100 group-hover/mongo:translate-y-0">
+                  Mongodb
+                </h1>
+              </li> */}
+              <li className="flex flex-col items-center gap-2 pb-2 group/mysql">
+                <Logo name="MySQL" size={40} />
+                <h1 className="text-xs transition-all duration-300 -translate-y-full opacity-0 group-hover/mysql:opacity-100 group-hover/mysql:translate-y-0">
+                  MySQL
+                </h1>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="flex flex-col gap-3">
+          <div className="flex flex-col gap-1">
+            <h1 className="text-sm">Skills Robotic </h1>
+          </div>
+          <div>
+            <ul className="grid justify-between grid-cols-4 2md:flex 2md:max-w-lg">
+              <li className="flex flex-col items-center gap-2 pb-2 group/c">
+                <Logo name="C Lang" size={40} />
+                <h1 className="text-xs transition-all duration-300 -translate-y-full opacity-0 group-hover/c:opacity-100 group-hover/c:translate-y-0">
+                  C Lang
+                </h1>
+              </li>
+              <li className="flex flex-col items-center gap-2 pb-2   group/c++">
+                <Logo name="C++" size={40} />
+                <h1 className="text-xs transition-all duration-300 -translate-y-full opacity-0 group-hover/c++:opacity-100 group-hover/c++:translate-y-0">
+                  C++
+                </h1>
+              </li>
+              <li className="flex flex-col items-center gap-2 pb-2 group/arduino">
+                <Logo name="Arduino" size={40} />
+                <h1 className="text-xs transition-all duration-300 -translate-y-full opacity-0 group-hover/arduino:opacity-100 group-hover/arduino:translate-y-0">
+                  Arduino
+                </h1>
+              </li>
+              <li className="flex flex-col items-center gap-2 pb-2 group/eagle">
+                <div>
+                  <Image src="/eagle.png" width={40} height={40} alt="eagle" />
+                </div>
+                <h1 className="text-xs transition-all duration-300 -translate-y-full opacity-0 group-hover/eagle:opacity-100 group-hover/eagle:translate-y-0">
+                  Eagle
+                </h1>
+              </li>
+              <li className="flex flex-col items-center gap-2 pb-2 group/inventor">
+                <div>
+                  <Image
+                    src="/inventor.png"
+                    width={40}
+                    height={40}
+                    alt="inventor"
+                  />
+                </div>
+                <h1 className="text-xs transition-all duration-300 -translate-y-full opacity-0 group-hover/inventor:opacity-100 group-hover/inventor:translate-y-0">
+                  Inventor
+                </h1>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-5 p-5 border 2md:p-8 border-slate-300 rounded-xl">
+        <div className="flex items-center gap-2 text-xl 2md:text-2xl ">
+          <Logo name={"rocket"} size={25} />
+          <h1 className="font-semibold">Let{"'"}s explore</h1>
+        </div>
+        <div>
+          <p className="text-justify">
+            Discover more about what I offer by exploring this portfolio or
+            check out my resume
+          </p>
+          <Link
+            href={
+              "https://drive.google.com/file/d/1anFrb3cxsGdulQFfnaEXpAbjW1jULciI/view?usp=sharing"
+            }
+            className={`inline-flex gap-2 items-center mt-4 ${
+              theme ? "bg-blue-300" : "bg-orange-500"
+            } py-2 px-5 rounded-lg hover:scale-105`}>
+            <Logo name={"arrowright"} size={20} />
+            My Resume
+          </Link>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default MainHome;
---
+++ components/MainProjectWeb.jsx
@@ -0,0 +1,146 @@
+import MainProjectWebDetail from "./MainProjectWebDetail";
+
+const obj = {
+  dashboardKit: {
+    img: "dashboard-kit.png",
+    name: "Dashboard Kit",
+    desc: "This application is a project that utilizes an API to display a list of customer tickets. It includes features to search for customers, pagination, filtering, and sorting by name.",
+    tech: ["Next.js", "Tailwindcss", "Redux Toolkit"],
+    linkDemo: "https://dashboard-kit-henna.vercel.app/",
+    linkCode: "/",
+  },
+  articleListApp: {
+    img: "article-list-app.png",
+    name: "Article List App",
+    desc: "This application is a project that utilizes an API to display articles. It includes a feature to search for articles from several countries, specific categories, and other general topics.",
+    tech: ["Next.js", "Tailwindcss"],
+    linkDemo: "https://article-list-one.vercel.app/",
+    linkCode: "/",
+  },
+  animeListApp: {
+    img: "anime-list-app.png",
+    name: "Anime List App",
+    desc: "This application is a project that utilizes an API to display a list of popular anime. It includes a feature to search for anime titles and a pagination system.",
+    tech: ["Next.js", "Tailwindcss"],
+    linkDemo: "https://anime-list-app-seven.vercel.app/",
+    linkCode: "/",
+  },
+  topicCRUD: {
+    img: "topic-crud.png",
+    name: "Topic CRUD",
+    desc: `This application is a simple project that features CRUD (Create, Read, Update, Delete) operations using Next.js, Tailwind CSS, and MongoDB as its database.`,
+    tech: ["Next.js", "Tailwindcss", "Mongodb"],
+    linkDemo: "https://cobafullstack.vercel.app/",
+    linkCode: "/",
+  },
+  catatanBelanja: {
+    img: "catatan-belanja.png",
+    name: "Shopping List",
+    desc: "This application is a simple project to display saved shopping lists. It includes a feature to filter existing shopping lists.",
+    tech: ["React.js", "Vite.js"],
+    linkDemo: "https://catatan-belanja-zeta.vercel.app/",
+    linkCode: "/",
+  },
+  responsifWeb: {
+    img: "responsif-web.png",
+    name: "Responsif Web",
+    desc: `An interactive and responsive web project with several supported animation views, built with modern Next.js technology.`,
+    tech: ["Next.js", "Tailwindcss", "Redux Toolkit", "Framer Motion"],
+    linkDemo: "https://asc-sf.vercel.app/",
+    linkCode: "/",
+  },
+  portfolio: {
+    img: "portfolio-v2.png",
+    name: "Portfolio",
+    desc: "Personal Website and portfolio built with Next.js and Tailwind CSS.",
+    tech: [
+      "Next.js",
+      "Tailwindcss",
+      "Redux Toolkit",
+      "Framer Motion",
+      "Mongodb",
+    ],
+    linkDemo: "https://portfolio-v2-rho-murex.vercel.app/",
+    linkCode: "/",
+  },
+  onlineShop: {
+    img: "onlineshopeeng.png",
+    name: "Online Shop",
+    desc: "A simple online shop application with features for category filtering, sorting, and searching for products, as well as adding items to a shopping cart that is saved to each user's account.",
+    tech: ["Laravel", "MySQL", "React.js", "Tailwindcss"],
+    linkDemo: "https://online-shop.vercel.app/",
+    linkCode: "/",
+  },
+};
+
+const MainProjectWeb = () => {
+  return (
+    <div className="grid w-full grid-cols-1 gap-0 py-8 md:grid-cols-2">
+      <MainProjectWebDetail
+        img={obj.dashboardKit.img}
+        name={obj.dashboardKit.name}
+        desc={obj.dashboardKit.desc}
+        tech={obj.dashboardKit.tech}
+        linkCode={obj.dashboardKit.linkCode}
+        linkDemo={obj.dashboardKit.linkDemo}
+      />
+      <MainProjectWebDetail
+        img={obj.portfolio.img}
+        name={obj.portfolio.name}
+        desc={obj.portfolio.desc}
+        tech={obj.portfolio.tech}
+        linkCode={obj.portfolio.linkCode}
+        linkDemo={obj.portfolio.linkDemo}
+      />
+      <MainProjectWebDetail
+        img={obj.onlineShop.img}
+        name={obj.onlineShop.name}
+        desc={obj.onlineShop.desc}
+        tech={obj.onlineShop.tech}
+        linkCode={obj.onlineShop.linkCode}
+        linkDemo={obj.onlineShop.linkDemo}
+      />
+      <MainProjectWebDetail
+        img={obj.responsifWeb.img}
+        name={obj.responsifWeb.name}
+        desc={obj.responsifWeb.desc}
+        tech={obj.responsifWeb.tech}
+        linkCode={obj.responsifWeb.linkCode}
+        linkDemo={obj.responsifWeb.linkDemo}
+      />
+      <MainProjectWebDetail
+        img={obj.articleListApp.img}
+        name={obj.articleListApp.name}
+        desc={obj.articleListApp.desc}
+        tech={obj.articleListApp.tech}
+        linkCode={obj.articleListApp.linkCode}
+        linkDemo={obj.articleListApp.linkDemo}
+      />
+      <MainProjectWebDetail
+        img={obj.animeListApp.img}
+        name={obj.animeListApp.name}
+        desc={obj.animeListApp.desc}
+        tech={obj.animeListApp.tech}
+        linkCode={obj.animeListApp.linkCode}
+        linkDemo={obj.animeListApp.linkDemo}
+      />
+      {/* <MainProjectWebDetail
+        img={obj.topicCRUD.img}
+        name={obj.topicCRUD.name}
+        desc={obj.topicCRUD.desc}
+        tech={obj.topicCRUD.tech}
+        linkCode={obj.topicCRUD.linkCode}
+        linkDemo={obj.topicCRUD.linkDemo}
+      /> */}
+      <MainProjectWebDetail
+        img={obj.catatanBelanja.img}
+        name={obj.catatanBelanja.name}
+        desc={obj.catatanBelanja.desc}
+        tech={obj.catatanBelanja.tech}
+        linkCode={obj.catatanBelanja.linkCode}
+        linkDemo={obj.catatanBelanja.linkDemo}
+      />
+    </div>
+  );
+};
+export default MainProjectWeb;
